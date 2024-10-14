e803ebb2e9f0ae74b4d9a3df6c0bf3e5a8433925
Yaroslav447 <126654722+Yaroslav447@users.noreply.github.com>
2024-10-14 23:04:19 +0300

    коммит 1
    первый коммит

---
+++ Lab1.1/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try // создаем проверку на ввод формата числа
+            {
+                int num1 = int.Parse(TextBox1.Text);
+                int num2 = int.Parse(TextBox2.Text);
+
+                int sum = (num1 + num2);
+                int proiz = (num1 * num2);
+
+                result.Text = "Результат: " +num1+ " + "+num2+ " = "+sum  
+                    +"                                                Pезультат: " + num1 + " " +" * " + num2 + " = " + proiz; 
+                //result.Text = "Результат: " + num1 + "*" + num2 + "=" + proiz;
+            }
+            catch
+            {
+                MessageBox.Show("Не верные данные. Попробуйте снова!");
+            }
+        }
+    }
+}
---
+++ Lab1.2/Lab1.2/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<string> people = new List<string>();// создаем список т.к. списк , так как массив должен быть заранее известен по размеру
+        string name;// переменная в которой храниться данные поля "имя"
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            name = inputtext.Text;
+            if (name != "")// проверка на пустое поле
+            {
+                if(people.Contains(name))// проверка на повторяющиеся эл. в списке
+                {
+                    inputtext.Clear();
+                    MessageBox.Show("Имя уже существует в списке");
+
+                }
+                else
+                {
+                    people.Add(name);// добавление обьекста в список
+                    inputtext.Clear();// очистка поля в которое мы вводили имя
+                    outtext.Text = string.Join(Environment.NewLine, people); // вывод в всего списка в текстблок
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поле имя пустое!");
+            }
+            
+            
+            //for (int i = 0; i < people.Count; i++)
+            //    {
+            //        outtext.Text = i.ToString();
+            //    }
+        }
+    }
+}
