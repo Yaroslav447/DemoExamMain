7aa6afbc3399d796adca0a54fa396bbdca050ae3
Yaroslav447 <126654722+Yaroslav447@users.noreply.github.com>
2024-10-27 18:31:14 +0300

    авторизация WPF

---
+++ avtoriz/avtoriz/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace avtoriz
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Content = new Page1();
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string loginUser;//= logbox.Text;
+        //    string passwordUser;// = passbox.Text;
+
+        //    DB db = new DB();
+           
+        //    DataTable table = new DataTable();
+            
+        //    MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+        //    MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @uL AND `pass`= @uP", db.getConnection());
+        //    //command.Parameters.Add("@uL",MySqlDbType.VarChar).Value =loginUser;
+        //    //command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = passwordUser;
+
+        //    adapter.SelectCommand = command;
+        //    adapter.Fill(table);
+
+
+        //    if (table.Rows.Count > 0)
+        //    {
+        //        MessageBox.Show("Готово");
+        //    }
+        //    else 
+        //    {
+        //        MessageBox.Show("Не верно");
+        //    }
+        //}
+    }
+}
---
+++ avtoriz/avtoriz/Page1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace avtoriz
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string loginUser= logbox.Text;
+            string passwordUser = passbox.Text;
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @uL AND `pass`= @uP", db.getConnection());
+            command.Parameters.Add("@uL",MySqlDbType.VarChar).Value =loginUser;
+            command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = passwordUser;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Готово");
+            }
+            else
+            {
+                MessageBox.Show("Не верно");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+    }
+}
